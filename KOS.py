9bbd91ccb3908269a937e9484efcd1a2163a9614
L4ncelot <78283095+KnightL4ncelot@users.noreply.github.com>
2021-03-07 02:27:58 +0300

    Python

--- KOS.py
+++ KOS.py
@@ -47,7 +47,7 @@ def attack():
         attack_num += 1
         packesnum =attack_num
         packesnum= str(packesnum)
-        print("[print(Fore.CYAN+'!')]Paketler Gönderiliyor-----> "+packesnum)
+        print(print(Fore.CYAN+"[!]")+"Paketler Gönderiliyor-----> "+packesnum)
         
         
         
